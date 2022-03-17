f84f03ada759224f08836ed2051230c6acc5619e
Ander <anderra57@gmail.com>
2022-03-17 02:47:55 +0100

    Cambios principales: Los datos ya se obtienen desde un JSON de la API Open Library y el Dialog ejecuta correctamente el método addBook()

--- app/src/main/java/com/anderpri/openlibganizer/MainActivity.java
+++ app/src/main/java/com/anderpri/openlibganizer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.anderpri.openlibganizer;
 
+import android.app.ProgressDialog;
 import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,19 +17,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements DialogNewBook.DialogNewBookListener { //implements DialogNewBook2.ListenerNewBook {
 
     private RecyclerView mRecyclerView;
-    private List<String> mTitles;
-    private List<Integer> mImages;
-    private MyAdapter adapter;
-    FloatingActionButton fab_main;
-    FloatingActionButton fab_settings;
-    FloatingActionButton fab_add;
-
+    private List<String> mTitles = new ArrayList<>();
+    private List<String> mImages = new ArrayList<>();
+    private MyAdapter adapter = new MyAdapter(this, mTitles, mImages);
+    private FloatingActionButton fab_main;
+    private FloatingActionButton fab_settings;
+    private FloatingActionButton fab_add;
+    private ProgressDialog pd;
 
     private boolean clicked = false;
 
@@ -34,56 +53,69 @@ public class MainActivity extends AppCompatActivity implements DialogNewBook.Dia
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         // ------------------------------------- //
         // RecyclerView + CardView configuration
 
+        // Las variables están inicializadas arriba
         mRecyclerView = findViewById(R.id.recyclerview);
 
-        mTitles = new ArrayList<>();
-        mImages = new ArrayList<>();
+        // Importamos la información de prueba, útil para hacer los testeos
+        importTestData();
 
-        adapter = new MyAdapter(this, mTitles,mImages);
-
-        mImages.add(R.drawable.a1);mImages.add(R.drawable.a2);mImages.add(R.drawable.a3);mImages.add(R.drawable.a4);mImages.add(R.drawable.a5);
-        mTitles.add("Uno");mTitles.add("Dos");mTitles.add("Tres");mTitles.add("Cuatro");mTitles.add("Cinco");
-
+        // Configuramos el tamaño de la grilla tanto en vertical como en horizontal
         int spanC;
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) { spanC = 4; } else { spanC = 2; }
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,spanC,GridLayoutManager.VERTICAL,false);
-
+        // Y finalmente
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, spanC, GridLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(adapter);
 
         // ------------------------------------- //
-        // Fixed Action Buttons
+        // Fixed Action Buttons (FAB)
 
         fab_main = findViewById(R.id.fab);
         fab_settings = findViewById(R.id.fab_settings);
         fab_add = findViewById(R.id.fab_add);
 
-        fab_main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onFABClicked();
-                //String text = "Main";
-                //Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-            }
+        fab_main.setOnClickListener(view -> {
+            onFABClicked();
+            // Toast.makeText(getApplicationContext(), "Main", Toast.LENGTH_SHORT).show();
         });
-        fab_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //String text = "Settings";
-                //Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-            }
+        fab_settings.setOnClickListener(view -> {
+            //Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
         });
-        fab_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialogNewBook();
-            }
-        });
+        fab_add.setOnClickListener(view -> openDialogNewBook());
+
+    }
+
+    private void importTestData() {
+
+        /*
+        String[] isbns = {"9780618680009","9780142000656",
+                "9780152052607","9780205609994","9780803740167"};
+
+        for (String s:isbns) {
+            addBook(s);
+        }*/
+
+        mTitles.add("The God Delusion");
+        mImages.add("https://covers.openlibrary.org/b/id/8231555-L.jpg");
+
+        mTitles.add("East of Eden");
+        mImages.add("https://covers.openlibrary.org/b/id/8309140-L.jpg");
+
+        mTitles.add("The Hundred Dresses");
+        mImages.add("https://covers.openlibrary.org/b/id/10703295-L.jpg");
+
+        mTitles.add("Influence");
+        mImages.add("https://covers.openlibrary.org/b/id/8284301-L.jpg");
+
+        mTitles.add("Roller Girl");
+        mImages.add("https://covers.openlibrary.org/b/id/11327078-L.jpg");
 
     }
 
@@ -94,15 +126,84 @@ public class MainActivity extends AppCompatActivity implements DialogNewBook.Dia
 
     @Override
     public void addBook(String mISBN) {
+        String uri = "https://openlibrary.org/api/books?bibkeys=ISBN:" + mISBN + "&jscmd=details&format=json";
+        new JsonTask().execute(uri);
+    }
 
-        String text = "Added new book";
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    private void addBookToRV(String JSON_STRING) {
 
-        mImages.add(R.drawable.a6);
-        mTitles.add(mISBN);
-        adapter.notifyDataSetChanged();
+        String mThumbnail = "N/A";
+        String mTitle = "N/A";
+        String mAuthor = "N/A";
+        String mPublisher = "N/A";
+        String mYear = "N/A";
+        String mISBN = "N/A";
 
-        //Toast.makeText(getApplicationContext(), username, Toast.LENGTH_SHORT).show();
+        try {
+
+            // Para esquivar la mala lectura de caracteres Unicode en los objetos
+            String JSON_STRING1 = forceUnicode(JSON_STRING);
+
+            System.out.println(JSON_STRING1);
+            System.out.println("-+-+-+-+-+-+");
+
+            JSONObject obj_root = new JSONObject(JSON_STRING1);
+
+            // En Java no es posible acceder directamente al primer elemento de un array y necesitamos
+            // saber el nombre exacto del elemento (el ISBN), para ello usamos este workaround
+            String isbn_search_temp = JSON_STRING1.split(": ")[0];
+            String isbn_search = isbn_search_temp.substring(2, isbn_search_temp.length() - 1);
+
+
+            JSONObject obj_main = obj_root.getJSONObject(isbn_search);
+            JSONObject obj_details = obj_main.getJSONObject("details");
+
+
+            try {mThumbnail = obj_main.getString("thumbnail_url")
+                    .replace("-S.jpg","-L.jpg")
+                    .replace("-M.jpg","-L.jpg");} catch (Exception e) { e.printStackTrace(); }
+            try {mTitle = obj_details.getString("title");} catch (Exception e) { e.printStackTrace(); }
+            try {mISBN = obj_details.getJSONArray("isbn13").getString(0);} catch (Exception e) { e.printStackTrace(); }
+
+        } catch (JSONException e) {
+            JSON_STRING = ""; // para que no entre en el if de después
+            Toast.makeText(getApplicationContext(), R.string.error_isbn, Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+
+        System.out.println("TITLE: " + mTitle);
+        System.out.println("ISBN: " + mISBN);
+
+
+        if (JSON_STRING.length() < 10) {
+            //Toast.makeText(getApplicationContext(), R.string.isbn_not_found, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), R.string.isbn_not_found, Toast.LENGTH_LONG).show();
+        } else {
+            mImages.add(mThumbnail);
+            mTitles.add(mTitle);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(getApplicationContext(), getString(R.string.book_added, mTitle), Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    private String forceUnicode(String JSON_STRING) {
+
+        // Fuente: https://stackoverflow.com/a/58455026
+
+        String UNICODE_REGEX = "\\\\u([0-9a-f]{4})";
+        Pattern UNICODE_PATTERN = Pattern.compile(UNICODE_REGEX);
+
+        Matcher matcher = UNICODE_PATTERN.matcher(JSON_STRING);
+        StringBuffer decodedMessage = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(
+                    decodedMessage, String.valueOf((char) Integer.parseInt(matcher.group(1), 16)));
+        }
+        matcher.appendTail(decodedMessage);
+        return decodedMessage.toString();
     }
 
     private void onFABClicked() {
@@ -113,7 +214,7 @@ public class MainActivity extends AppCompatActivity implements DialogNewBook.Dia
 
     private void setVisibility() {
 
-        if (!clicked){
+        if (!clicked) {
             fab_add.setVisibility(View.VISIBLE);
             fab_settings.setVisibility(View.VISIBLE);
         } else {
@@ -123,12 +224,12 @@ public class MainActivity extends AppCompatActivity implements DialogNewBook.Dia
     }
 
     private void setAnimation() {
-        Animation rotateOpen = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_open_anim);
-        Animation rotateClose = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_close_anim);
-        Animation toBottom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.to_bottom_anim);
-        Animation fromBottom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.from_bottom_anim);
+        Animation rotateOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_open_anim);
+        Animation rotateClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_close_anim);
+        Animation toBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_bottom_anim);
+        Animation fromBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.from_bottom_anim);
 
-        if (!clicked){
+        if (!clicked) {
             fab_add.startAnimation(fromBottom);
             fab_settings.startAnimation(fromBottom);
             fab_main.startAnimation(rotateOpen);
@@ -138,20 +239,61 @@ public class MainActivity extends AppCompatActivity implements DialogNewBook.Dia
             fab_main.startAnimation(rotateClose);
         }
     }
-/*
-    @Override
-    public void alpulsarSI() {
-        String uri = "https://www.google.com/";
-        Log.i("etiqueta", uri);
-        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        startActivity(i);
-    }
 
-    @Override
-    public void alpulsarNO() {
-        String uri = "https://www.bing.com/";
-        Log.i("etiqueta", uri);
-        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        startActivity(i);
-    }*/
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        // Fuente: https://stackoverflow.com/a/37525989
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuilder buffer = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                    Log.d("Response: ", "> " + line);
+                }
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()) {
+                pd.dismiss();
+            }
+            addBookToRV(result);
+        }
+    }
 }
