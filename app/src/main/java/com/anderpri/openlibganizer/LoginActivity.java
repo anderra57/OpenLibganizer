f3929faa6ce7b3440a0b5afa8653f18a5133ef4a
Ander <anderra57@gmail.com>
2022-03-25 00:49:03 +0100

    Login ya disponible desde base de datos

--- app/src/main/java/com/anderpri/openlibganizer/LoginActivity.java
+++ app/src/main/java/com/anderpri/openlibganizer/LoginActivity.java
@@ -3,11 +3,24 @@ package com.anderpri.openlibganizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.anderpri.openlibganizer.db.AppDatabase;
+import com.anderpri.openlibganizer.db.User;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,25 +31,47 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.setLocale(this,"en");
+        
+        addUser();
+
         setContentView(R.layout.activity_login);
 
         mUserText = findViewById(R.id.login_username);
         mPassText = findViewById(R.id.login_password);
         mButton = findViewById(R.id.login_button);
 
-        mButton.setOnClickListener(view -> comprobarLogin(view));
+        mButton.setOnClickListener(view -> checkLogin(view));
 
     }
 
-    private void comprobarLogin(View view) {
+    private void addUser() {
+        AppDatabase db  = AppDatabase.getInstance(this.getApplicationContext());
+
+        String input_user = "a";
+        String input_pass = "a";
+
+        String lang = "es"; // default language, cambiar
+        boolean theme = false; // default
+
+        if (!db.userDao().userExists(input_user)){
+            User user = new User(input_user,input_pass,lang,theme);
+            db.userDao().insertUser(user);
+        }
+    }
+
+    private void checkLogin(View view) {
         String sUser = mUserText.getText().toString();
         String sPass = mPassText.getText().toString();
 
         System.out.println(sUser + " " + sPass);
+        boolean basicLogin = sUser.equals("") && sPass.equals("");
+        User u = login(sUser,sPass);
 
-        if (sUser.equals("") && sPass.equals("")){ // de prueba
+        if (basicLogin || (u != null)){ // de prueba
             // entrar
             Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            i.putExtra("username",sUser);
             finish();
             startActivity(i);
         } else {
@@ -44,5 +79,10 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(view.getContext(),R.string.login_incorrect_creds,Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private User login(String sUser, String sPass) {
+        AppDatabase db  = AppDatabase.getInstance(this.getApplicationContext());
+        return db.userDao().getUser(sUser, sPass);
     }
 }
