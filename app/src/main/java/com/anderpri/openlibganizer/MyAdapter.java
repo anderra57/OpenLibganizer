4088173171a5039b0d5f7983dcd12800f82ae9fc
Ander <anderra57@gmail.com>
2022-03-25 01:11:08 +0100

    Migrar el Adapter a la nueva clase Books

--- app/src/main/java/com/anderpri/openlibganizer/MyAdapter.java
+++ app/src/main/java/com/anderpri/openlibganizer/MyAdapter.java
@@ -1,7 +1,9 @@
 package com.anderpri.openlibganizer;
 
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,26 +13,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    private Context context;
-    //private List<String> titles;
-    //private List<String> images;
-    private List<Book> books;
-    private OnBookListener onBookListener;
+    private final Context context;
+    private final Books books;
+    private final OnBookListener onBookListener;
 
-    public MyAdapter(Context context, List<Book> books,OnBookListener onBookListener ){ // List<String> titles, List<String> images){
+    public MyAdapter(Context context, Books books,OnBookListener onBookListener ){
         this.context=context;
         this.books=books;
         this.onBookListener=onBookListener;
-        //this.images=images;
-        //this.titles=titles;
     }
 
     @NonNull
@@ -43,7 +46,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.mTextView.setText(books.get(position).getmTitle());
-        if (!books.get(position).getmThumbnail().equals("N/A")){
+        if (!books.get(position).getmThumbnail().equals(context.getString(R.string.default_info))){
             // Fuente: https://devexperto.com/glide-android/
             Uri uri = Uri.parse(books.get(position).getmThumbnail());
             Glide.with(context).load(uri).into(holder.mImageView);
@@ -60,9 +63,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        OnBookListener onBookListener;
-        ImageView mImageView;
-        TextView mTextView;
+        final OnBookListener onBookListener;
+        final ImageView mImageView;
+        final TextView mTextView;
 
         public MyViewHolder(@NonNull View itemView, OnBookListener onBookListener) {
             super(itemView);
@@ -72,16 +75,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             this.onBookListener=onBookListener;
 
             itemView.setOnClickListener(this);
+            //Toast.makeText(view.getContext(),"Clicked on: "+ getAdapterPosition(),Toast.LENGTH_SHORT).show();
 
-            /*
-            itemView.setOnClickListener(view -> {
-
-                onBookListener.onBookClick(getAdapterPosition());
-
-
-
-                //Toast.makeText(view.getContext(),"Clicked on: "+ getAdapterPosition(),Toast.LENGTH_SHORT).show();
-            });*/
         }
 
         @Override
